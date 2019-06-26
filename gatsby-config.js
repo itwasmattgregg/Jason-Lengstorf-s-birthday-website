3de862d90ed367d8c4ffca86b3a5ce570a4610c5
Matt Gregg <mgregg@nerdery.com>
2019-06-26 14:23:18 -0500

    Initial commit

---
+++ gatsby-config.js
@@ -0,0 +1,34 @@
+module.exports = {
+  siteMetadata: {
+    title: `Jason Lengstorf's Official Birthday Website`,
+    description: `A site made with special Gatsby ❤️ for Jason Lengstorf as special thanks for all he does for our community.`,
+    author: `@itwasmattgregg`,
+  },
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images`,
+      },
+    },
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `gatsby-starter-default`,
+        short_name: `starter`,
+        start_url: `/`,
+        background_color: `#663399`,
+        theme_color: `#663399`,
+        display: `minimal-ui`,
+        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+      },
+    },
+    // this (optional) plugin enables Progressive Web App + Offline functionality
+    // To learn more, visit: https://gatsby.dev/offline
+    // `gatsby-plugin-offline`,
+  ],
+}
